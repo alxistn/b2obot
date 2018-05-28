5a1cb74cce819fc1462321465d1687114342881d
Louison Dumont <louison@power.technology>
2018-05-28 16:45:08 -0700

    clean depot

--- train_gigantic.py
+++ train_gigantic.py
@@ -8,11 +8,13 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.layers import RNN
 from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
 
 
 # Loading data
 
-text = (open("LyricsGenius/format_dataset.js").read())
+text = (open("LyricsGenius/Lyrics_Booba.txt").read())
+#text = (open("debug.txt").read())
 text=text.lower()
 
 
@@ -53,36 +55,48 @@ model.add(LSTM(700))
 model.add(Dropout(0.2))
 model.add(Dense(Y_modified.shape[1], activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='adam',  metrics=['accuracy'])
 
+# checkpointPath = "weights-improvement-{epoch:02d}-{acc:.2f}.h5"
+# checkpoint = ModelCheckpoint(checkpointPath, monitor='acc', save_best_only=True, mode='max', period=1)
 
-model.fit(X_modified, Y_modified, epochs=100, batch_size=50)
+# callbacks_list = [checkpoint]
 
-model.save_weights('text_generator_gigantic.h5')
+# model.fit(X_modified, Y_modified, epochs=100, batch_size=150, callbacks=callbacks_list)
 
-#model.load_weights('text_generator_gigantic.h5')
+# model.save_weights('text_generator_gigantic.h5')
+
+model.load_weights('text_generator_gigantic.h5')
 
 
 # Generating text
 
-string_mapped = X[99]
-full_string = [n_to_char[value] for value in string_mapped]
-# generating characters
-for i in range(400):
-    x = np.reshape(string_mapped,(1,len(string_mapped), 1))
-    x = x / float(len(characters))
+def make_prediction(length):
+    print ("########## " + `length` + " ##########")
+    string_mapped = X[np.random.randint(0, len(X) - 1)]
+    full_string = [n_to_char[value] for value in string_mapped]
+    print ("Seed: " + ''.join(full_string) + " ----------\n")
+    # generating characters
+    for i in range(length):
+        x = np.reshape(string_mapped,(1,len(string_mapped), 1))
+        x = x / float(len(characters))
 
-    pred_index = np.argmax(model.predict(x, verbose=0))
-    seq = [n_to_char[value] for value in string_mapped]
-    full_string.append(n_to_char[pred_index])
+        pred_index = np.argmax(model.predict(x, verbose=0))
+        seq = [n_to_char[value] for value in string_mapped]
+        full_string.append(n_to_char[pred_index])
 
-    string_mapped.append(pred_index)
-    string_mapped = string_mapped[1:len(string_mapped)]
+        string_mapped.append(pred_index)
+        string_mapped = string_mapped[1:len(string_mapped)]
 
 
-# Combining text
+    # Combining text
+        
+    txt=""
+    for char in full_string:
+        txt = txt+char
+    print (txt)
 
-txt=""
-for char in full_string:
-    txt = txt+char
-print (txt)
+
+for i in range(100):
+    make_prediction(144)
+
